26a46616e6a6195ce0c921bf0e7b3acdf1a207d4
Yannick Burkhardt <yannick.burkhardt@tum.de>
2022-07-14 20:29:02 +0200

    clean up

--- nav2D-envs/main.py
+++ nav2D-envs/main.py
@@ -1,21 +1,23 @@
-import nav2D_envs
 import gym
-import random
 import numpy as np
 
+# Set up environment
 env = gym.make('nav2D_envs/Nav2DWorld-v0')
 observation, info = env.reset(seed=42, return_info=True)
-action = np.zeros(2) #np.random.randint(-1,1,size=(2,))
-for _ in range(100):
-    env.render(mode="human") #env.render(mode="rgb_array")
-    observation, reward, done, info = env.step(action)
-    #goal = observation['desired_goal']
-    #pos = observation['achieved_goal'][:2]
+action = np.zeros(2)
 
+# Run test
+for _ in range(100):
+    env.render(mode="human")
+    observation, reward, done, info = env.step(action)
+
+    # Move agent towards goal
     direction = observation[2:4]
     action = direction/np.linalg.norm(direction)/256*10
     print(observation)
+
     if done:
+        # Reset environment
         observation, info = env.reset(return_info=True)
         action = np.zeros(2)
 
